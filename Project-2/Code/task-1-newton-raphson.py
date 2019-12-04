267f255a767668e129871abf11ab648ea2405095
Fantomas4 <stefanokf@auth.gr>
2019-12-05 00:31:33 +0200

    Worked on Project-2

---
+++ Project-2/Code/task-1-newton-raphson.py
@@ -0,0 +1,14 @@
+# Calculates the value of function f for x
+import math
+
+
+def calculate_f(x):
+    return math.exp(math.pow(math.sin(x), 3)) + math.pow(x, 6) - 2 * math.pow(x, 4) - math.pow(x, 3) - 1
+
+# Calculates the value of the derivative of function f for x
+def calculate_der_f(x):
+    return ()
+
+
+if __name__ == "__main__":
+
