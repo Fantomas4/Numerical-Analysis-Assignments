ead3cd684eea2a426d2cc7f2c66e12e1ecf5a708
Fantomas4 <stefanokf@auth.gr>
2019-12-11 01:27:01 +0200

    Updated the code for task 3.

--- Project-2/Code/task-3/pa_lu_gauss.py
+++ Project-2/Code/task-3/pa_lu_gauss.py
@@ -1,9 +1,7 @@
-
 # The function takes the A matrix (2D, square) as input and produces a unit lower triangular
 # matrix L, an upper triangular matrix U and a permutation vector P, so that
 # P * A = L * U
 def perform_pa_lu_decomposition(a_matrix):
-
     # Calculate the size of A matrix
     a_size = len(a_matrix)
 
@@ -30,9 +28,9 @@ def perform_pa_lu_decomposition(a_matrix):
 
     for col in range(a_size):
         # Find the maximum element of the pivot's column,
-        # searching from the pivot element and below
-        max_pos = pivot_pos
-        max_value = a_matrix[pivot_pos[0]][pivot_pos[1]]
+        # searching from the diagonal's element in the pivot's column and below
+        max_pos = [pivot_pos[1], pivot_pos[1]]
+        max_value = a_matrix[max_pos[0], max_pos[1]]
 
         for line in range(pivot_pos[0] + 1, a_size):
             if a_matrix[line][col] > max_value:
@@ -51,6 +49,29 @@ def perform_pa_lu_decomposition(a_matrix):
             # Swap the rows in A matrix
             a_matrix[pivot_pos[0]] = max_row
             a_matrix[max_pos[0]] = pivot_row
+
+            # Update pivot_pos value
+            pivot_pos = max_pos
+
+        # Make all elements below the main diagonal element zero.
+        # To do that, we will make matrix calculations between the pivot's row
+        # and each of the target rows
+
+        # First, find the diagonal element of our column
+        diag_elem_pos = [pivot_pos[0], pivot_pos[0]]
+
+        # Get the whole row where the pivot is located
+        pivot_row = a_matrix[pivot_pos[0]]
+
+        for row in range(pivot_pos[0] + 1, a_size):
+            # Get the column element's whole row
+            target_row = a_matrix[row]
+
+            # For each element of the target row, perform the matrix calculations
+
+
+
+
 
 
 
