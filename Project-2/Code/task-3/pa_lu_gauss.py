25dbce7f9aa08e0d08a40c7b26d16997290b8ad8
Fantomas4 <Fantomas4@users.noreply.github.com>
2019-12-13 17:57:44 +0200

    Worked on a third (and hopefully last...) revision of the PA = LU algorithm.

--- Project-2/Code/task-3/pa_lu_gauss.py
+++ Project-2/Code/task-3/pa_lu_gauss.py
@@ -1,123 +1,85 @@
-# The function takes the A matrix (2D, square) as input and produces a unit lower triangular
-# matrix L, an upper triangular matrix U and a permutation vector P, so that
-# P * A = L * U
-def perform_pa_lu_decomposition(a_matrix):
+def mult_matrix(M, N):
+    """Multiply square matrices of same dimension M and N"""
+    # Create an empty result matrix filled with 0's
+    result_matrix = []
+    for i in range(len(M)):
+        result_matrix.append([0] * len(N[0]))
 
-    # A matrix is a list of lists, each representing a row
-    print("Input-> A matrix is:")
-    for line in a_matrix:
-        print(*line)
+    for i in range(len(M)):
+        m_row = M[i]
+        for j in range(len(N[0])):
+            n_column = []
+            for line in range(len(N)):
+                n_column.append(N[line][j])
 
-    # Calculate the size of A matrix
-    a_size = len(a_matrix)
+            for k in range(len(m_row)):
+                result_matrix[i][j] += m_row[k] * n_column[k]
 
-    # # Create the permutation matrix P
-    # p_matrix = []
-    #
-    # for col in range(a_size):
-    #
-    #     cur_col = []
-    #
-    #     for line in range(a_size):
-    #         if col == line:
-    #             cur_col.append(1)
-    #         else:
-    #             cur_col.append(0)
-    #
-    #     p_matrix.append(cur_col)
-
-    # Create a vector representing the permutation vector P
-    p_vector = [i for i in range(a_size)]
-
-    for col in range(a_size):
-
-        # The current pivot element's position in [row, column] format
-        pivot_pos = [col, col]
-
-        for row in range(pivot_pos[0] + 1, a_size):
-
-            # Sort the column elements as in descending order,
-            # by swapping the elements' rows, starting from the pivot's row and below
-
-
-            # Find the maximum element of the pivot's column,
-            # searching from the pivot element's position and below
-            max_pos = [pivot_pos[0], pivot_pos[1]]
-            max_value = a_matrix[max_pos[0]][max_pos[1]]
-
-            for line in range(pivot_pos[0] + 1, a_size):
-                if abs(a_matrix[line][col]) > abs(max_value):
-                    max_value = a_matrix[line][col]
-                    max_pos = [line, col]
-
-            # If an element below the pivot is found to have a greater value than it,
-            # swap the pivot's row with the max element's row
-            if max_pos != pivot_pos:
-                # Get the pivot's row
-                # pivot_row = [a_matrix[pivot_pos[0]][k] for k in range(a_size)]
-                pivot_row = a_matrix[pivot_pos[0]]
-
-                # Get the max element's row
-                max_row = a_matrix[max_pos[0]]
-
-                # Swap the rows in A matrix
-                a_matrix[pivot_pos[0]] = max_row
-                a_matrix[max_pos[0]] = pivot_row
-
-                # Modify the permutation vector P
-                temp = p_vector[pivot_pos[0]]
-                p_vector[pivot_pos[0]] = p_vector[max_pos[0]]
-                p_vector[max_pos[0]] = temp
-
-            # Make all elements below the pivot element zero.
-            # To do that, we will make matrix calculations between the pivot's row
-            # and each of the target rows
-
-            # Get the pivot element's value
-            pivot_elem = a_matrix[pivot_pos[0]][pivot_pos[1]]
-
-            # Get the whole row in which the pivot is located
-            pivot_row = a_matrix[pivot_pos[0]]
-
-            for r in range(pivot_pos[0] + 1, a_size):
-                # Get the column element's whole row
-                target_row = a_matrix[r]
-
-                # Get the first element of target row that is below the diagonal's current pivot
-                target_first_elem = target_row[pivot_pos[1]]
-
-                # For each element of the target row, starting from the element at the same column as the pivot,
-                # perform the matrix calculations
-                for elem in range(pivot_pos[1], len(target_row)):
-                    # target_row[elem] = target_row[elem] + pivot_elem * (- target_row[pivot_pos[1]] / pivot_elem)
-                    target_row[elem] = target_row[elem] + pivot_row[elem] * (- target_first_elem / pivot_elem)
-
-                # Place the modified row back to A matrix
-                a_matrix[r] = target_row
-
-    print("\n\nResult-> A matrix is: ")
-    for line in a_matrix:
-        print(*line)
-
-# test_matrix = []
-# test_matrix.append([2, 4, 1])
-# test_matrix.append([-1, -2, 2])
-# test_matrix.append([4, 2, -3])
-test_matrix = []
-test_matrix.append([1.383, 0.1, 0.5])
-test_matrix.append([0.0077, 0.1, 0.1976])
-test_matrix.append([-2.6452, -1.383, 1.9127])
-
-perform_pa_lu_decomposition(test_matrix)
+    return result_matrix
 
 
 
+def pivot_matrix(M):
+    """Returns the pivoting matrix for M, used in Doolittle's method."""
+    m = len(M)
+
+    # Create an identity matrix, with floating point values
+    id_mat = [[float(i ==j) for i in range(m)] for j in range(m)]
+
+    # Rearrange the identity matrix such that the largest element of
+    # each column of M is placed on the diagonal of M
+    for j in range(m):
+        row = max(range(j, m), key=lambda i: abs(M[i][j]))
+        if j != row:
+            # Swap the rows
+            id_mat[j], id_mat[row] = id_mat[row], id_mat[j]
+
+    return id_mat
+
+def lu_decomposition(A):
+    """Performs an LU Decomposition of A (which must be square)
+    into PA = LU. The function returns P, L and U."""
+    n = len(A)
+
+    # Create zero matrices for L and U
+    L = [[0.0] * n for i in range(n)]
+    U = [[0.0] * n for i in range(n)]
+
+    # Create the pivot matrix P and the multipled matrix PA
+    P = pivot_matrix(A)
+
+    #BUG IN LINE 39! PA = mult_matrix(P, A)
+    PA = mult_matrix(P, A)
+
+    # Perform the LU Decomposition
+    for j in range(n):
+        # All diagonal entries of L are set to unity
+        L[j][j] = 1.0
+
+        # LaTeX: u_{ij} = a_{ij} - \sum_{k=1}^{i-1} u_{kj} l_{ik}
+        for i in range(j+1):
+            s1 = sum(U[k][j] * L[i][k] for k in range(i))
+            U[i][j] = PA[i][j] - s1
+
+        # LaTeX: l_{ij} = \frac{1}{u_{jj}} (a_{ij} - \sum_{k=1}^{j-1} u_{kj} l_{ik} )
+        for i in range(j, n):
+            s2 = sum(U[k][j] * L[i][k] for k in range(j))
+            L[i][j] = (PA[i][j] - s2) / U[j][j]
+
+    return (P, L, U)
 
 
+A = [[7, 3, -1, 2], [3, 8, 1, -4], [-1, 1, 4, -1], [2, -4, -1, 6]]
+P, L, U = lu_decomposition(A)
 
+print("A:")
+print(A)
 
+print("P:")
+print(P)
 
+print("L:")
+print(L)
 
-
-
-
+print("U:")
+print(U)
