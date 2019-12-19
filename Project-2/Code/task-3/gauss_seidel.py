96cdce02186a0e0e8f80fd776dd3324a90af5336
Fantomas4 <Fantomas4@users.noreply.github.com>
2019-12-19 11:37:01 +0200

    Finalized gauss_seidel.py

--- Project-2/Code/task-3/gauss_seidel.py
+++ Project-2/Code/task-3/gauss_seidel.py
@@ -1,9 +1,8 @@
 # t_err specifies the required precision for our calculations.
 # A value of 0.00005 specifies a precision of 4 floating point numbers
 # (with rounding).
-#t_err = 0.00005
-# FOR TESTING ONLY!!!
-t_err = 0.005
+t_err = 0.00005
+
 
 def calculate_vector_diff(vector_1, vector_2):
     """ Takes 2 vectors and returns a new vector, which is the result of the subtraction
@@ -35,9 +34,6 @@ def generate_a_matrix(n):
     b_vector[0] = 3
     b_vector[n - 1] = 3
 
-    #FOR TESTING ONLY!!!!!
-    b_vector = [10, 10, 10]
-
     for i in range(n):
         for j in range(n):
             if i == j:
@@ -46,14 +42,6 @@ def generate_a_matrix(n):
             elif i+1 == j or i == j+1:
                 # Case A(i+1,i) = A(i,i+1) = -2
                 a_matrix[i][j] = -2
-
-    # FOR TESTING ONLY!!!
-    a_matrix = [[8, 1, 1], [1, 8, 1], [1, 1, 8]]
-
-    print("b_vector is: ", b_vector)
-    print("a_matrix is: ")
-    for line in a_matrix:
-        print(line)
 
     while True:
         old_x_vector = x_vector.copy()
@@ -72,5 +60,8 @@ def generate_a_matrix(n):
     return x_vector
 
 
-result = generate_a_matrix(3)
-print("Result returned is: ", result)
+print("\n*** Executing for n = 10. Please wait... ***")
+print("=> X vector for n = 10 is: ", generate_a_matrix(10))
+print("\n*** Executing for n = 10000. Please wait... ***")
+print("=> X vector for n = 10000 is: ", generate_a_matrix(10000))
+
